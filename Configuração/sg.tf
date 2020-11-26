b2cba5bcf4ac61b21d914f28c8897abfa09cb8a8
saulosilva85 <57241516+saulosilva85@users.noreply.github.com>
2020-11-25 22:11:39 -0300

    Add files via upload

---
+++ Configuração/sg.tf
@@ -0,0 +1,99 @@
+Configurado os grupos de segurança
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "server 01" {
+  name        = "server 01"
+  description = "Trafego entre os server 01"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Kubernetes API server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.k8s.cidr_block]
+  }
+  
+  ingress {
+    description = "etcd server client API"
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.k8s.cidr_block]
+  }
+
+  ingress {
+    description = "Kubelet API - schedule - manager"
+    from_port   = 10250
+    to_port     = 10252
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.k8s.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "server 01"
+  }
+}
+
+resource "aws_security_group" "server 02" {
+  name        = "server 02"
+  description = "Trafego entre os server 02"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Kubelet API"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.k8s.cidr_block]
+  }
+
+ingress {
+    description = "NodePort Services"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.k8s.cidr_block]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "server 02"
+  }
+}
